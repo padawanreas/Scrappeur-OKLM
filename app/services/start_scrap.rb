2ccdb6b015378918c16215e21f2dfc9ce2ecb13c
rebeccabosse <rebeccabosse@hotmail.fr>
2018-11-08 18:30:20 +0100

    modif route.rb

--- app/services/start_scrap.rb
+++ app/services/start_scrap.rb
@@ -7,12 +7,12 @@ PAGE_URL ="https://coinmarketcap.com/"
 class StartScrap
 
   def initialize
-    @page = Nokogiri::HTML(open("https://coinmarketcap.com")) #recuper le nom de la monnaie
+    @page = Nokogiri::HTML(open("https://coinmarketcap.com"))
   end
 
   def perform
-    @crypto = @page.xpath('//*[contains(@class,"currency-name-container link-secondary")]').map{|item| item.text} #le nom de la monnaie
-    @value= @page.xpath('//*[contains(@class,"no-wrap text-right")]').map{ |item| "$#{item["data-sort"]}"} # prix de la monnaie
+    @crypto = @page.xpath('//*[contains(@class,"currency-name-container link-secondary")]').map{|item| item.text}
+    @value= @page.xpath('//*[contains(@class,"no-wrap text-right")]').map{ |item| "$#{item["data-sort"]}"}
     @result=[]
       n=0
       while n < @crypto.length
